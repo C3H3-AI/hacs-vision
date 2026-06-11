c66be438e11166e4528003cf3e37ea7b635e7384
C3H3-AI <C3H3-AI@users.noreply.github.com>
2026-06-11 21:02:21 +0800

    清理: 移除开发文件, 只保留安装必需

--- frontend_src/src/index.js
+++
@@ -1,9 +0,0 @@
-import { HacsVisionPanel } from './hacs-vision-panel.js';
-import './views/browse.js';
-import './views/updates.js';
-import './views/management.js';
-import './views/config-view.js';
-import './views/integrations-list.js';
-import './components/config-flow-dialog.js';
-
-customElements.define('hacs-vision-panel', HacsVisionPanel);
